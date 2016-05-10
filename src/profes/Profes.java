57e0582fce926511b45420e94ea6b0cdfc16a9b7
sandrinhosbrown <sandrinhosbrown@gmail.com>
2016-05-10 14:15:34 +0200

    Iniciamos el proyecto

---
+++ src/profes/Profes.java
@@ -0,0 +1,19 @@
+/*
+ * Aplicacion de Profesores de un centro
+ */
+package profes;
+
+/**
+ *
+ * @author usu21
+ */
+public class Profes {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
